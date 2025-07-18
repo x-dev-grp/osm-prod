775b5db833e8f1d1ce522593e8ad0239b7dc163b
WissemMlouka <wissem.mlouka1235@gmail.com>
2025-07-18 21:03:13 +0100

    multi tenancy

--- src/main/java/com/osm/oilproductionservice/repository/CompanyProfileRepository.java
+++
@@ -1,7 +0,0 @@
-package com.osm.oilproductionservice.repository;
-
-import com.osm.oilproductionservice.model.CompanyProfile;
-import com.xdev.xdevbase.repos.BaseRepository;
-
-public interface CompanyProfileRepository extends BaseRepository<CompanyProfile> {
-}
