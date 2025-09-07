d8aa58a32bbcd1690a1b06da68551f5153c5b76f
Smail <ismailmansouri571@gmail.com>
2025-09-07 15:33:11 +0100

    edit ci cd

---
+++ src/main/java/com/osm/oilproductionservice/enums/OperationType.java
@@ -0,0 +1,12 @@
+package com.osm.oilproductionservice.enums;
+
+public enum OperationType {
+    BASE,
+    OLIVE_PURCHASE,
+    OIL_PURCHASE,
+    OIL_SALE,
+    PAYMENT,
+    EXCHANGE,
+    SIMPLE_RECEPTION,
+    INTERNAL_RECEPTION,
+}
